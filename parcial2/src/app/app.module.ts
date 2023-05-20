e6e8fac83d151fd5499edca4c32c7368dcab96d1
David Fuquen <d.fuquen@uniandes.edu.co>
2023-05-19 19:37:53 -0500

    Listo pt1

--- parcial2/src/app/app.module.ts
+++ parcial2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import {BandModule} from './band/band.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BandModule
+    BandModule,
+    HttpClientModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- parcial2/src/app/band/band-list/band-list.component.ts
+++ parcial2/src/app/band/band-list/band-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Band } from '../band';
+import { BandService } from '../band.service';
 
 @Component({
   selector: 'app-band-list',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./band-list.component.css']
 })
 export class BandListComponent implements OnInit {
+  bands: Array<Band> = [];
+  oldie: number = 0;
+  oldie_band: string = "";
+  selected: Boolean =  false;
 
-  constructor() { }
+  selectedSerie!: Band;
+
+  constructor(private bandService: BandService) { }
+
+  getBands(): void {
+    this.bandService.getBands().subscribe((bands) => {
+      this.bands = bands;
+
+
+      let max: number = 0;
+      let maxBand: string = "";
+      bands.map((s) => {
+        let actual = s.foundation_year;
+        let resta = 2023 - actual;
+        if (resta > max) {
+          max = resta;
+          maxBand = s.name;
+        }
+      });
+      this.oldie = max;
+      this.oldie_band = maxBand;
+    });
+  }
 
   ngOnInit() {
+    this.getBands();
   }
 
 }
